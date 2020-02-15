038703e7a69a77b5c0f0b9a397d5090fec0ef30c
SebastjanLeskovar <sebastjan.leskovar@gmail.com>
2020-02-15 14:11:37 +0100

    5.4

--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
+from .apiviews import ChoiceList, CreateVote, PollDetail, PollList
 from .views import polls_detail, polls_list
 
 urlpatterns = [
-    path("polls/", polls_list, name="polls_list"),
-    path("polls/<int:pk>/", polls_detail, name="polls_detail")
+    path("polls/", PollList.as_view(), name="polls_list"),
+    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+    path("choices/", ChoiceList.as_view(), name="choice_list"),
+    path("vote/", CreateVote.as_view(), name="create_vote"),
 ]
